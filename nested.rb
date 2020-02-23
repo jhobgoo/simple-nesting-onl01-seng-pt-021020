c58e368ac8f4397e5d72571c06d0366d780a260a
Joel Hobgood <jhobgoo@gmail.com>
2020-02-23 02:30:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,8 +70,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
- 		{   
+	programmer_hash = {   
  	        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
