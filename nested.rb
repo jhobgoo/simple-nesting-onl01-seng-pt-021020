1533a9313800145c23d70db629a03a71c9ad7a6e
Joel Hobgood <jhobgoo@gmail.com>
2020-02-23 02:21:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,10 +86,7 @@ def adding_matz
         }
      }
      
-    programmer_hash[:yukihiro_matsumoto][:known_for][:languages]
-      :known_for => "Ruby"
-      :languages => "LISP", "C"
-    programmer_hash
+    programmer_hash[:yukihiro_matsumoto]
     
 end
 
